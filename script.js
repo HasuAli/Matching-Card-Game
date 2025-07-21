8164d5b8e760c70e49e77219fef680697a4f89db
Muhammad Hasnain <hasuali809@gmail .com>
2025-07-21 02:03:29 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,91 @@
+const images = ["🍎", "🍌", "🍇", "🍉", "🍓", "🥝", "🥭", "🍑"]
+
+// shallow copy with Crad 
+const gameCard = [...images, ...images]
+gameCard.sort(() => 0.5 - Math.random())
+
+const gameBox = document.querySelector('.gamebox')
+let firstCard, secondCard ;
+let lockBoard = false
+let matchPair = 0
+
+function Createcard(item) {
+    const cards = document.createElement('div')
+    cards.classList.add('card')
+    cards.dataset.name = item
+
+    cards.innerHTML = `
+         <div class="front">${item}</div>
+         <div class="back"></div>
+        `
+    cards.addEventListener('click', flipCard)
+    return cards
+}
+
+function setUpGame() {
+    gameCard.forEach(element => {
+        const Card = Createcard(element)
+        gameBox.appendChild(Card)
+    }
+    )
+
+}
+
+function flipCard() {
+    if (lockBoard) return 
+    if (this === firstCard) {
+        return
+    }
+
+    this.classList.add('flip');
+
+    if (!firstCard) {
+        firstCard = this
+        return
+    }
+    secondCard = this
+    lockBoard = true;
+
+    checkMatch()
+}
+function checkMatch() {
+    let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+
+    if (isMatch) {
+       disableCard()
+    } else {
+        CardUnFlip()
+    }
+}
+
+function disableCard() {
+    firstCard.removeEventListener('click', flipCard)
+    secondCard.removeEventListener('click', flipCard)
+
+    matchPair++
+    if (matchPair === gameCard.length) {
+        document.getElementById('massage').textContent = '🎊 You Win game'
+    }
+    resetGame()
+}
+
+function CardUnFlip() {
+    setTimeout(() => {
+        firstCard.classList.remove('flip')
+        secondCard.classList.remove('flip')
+         resetGame()
+    },1000);
+
+}
+function resetGame() {
+    [firstCard, secondCard] = [null, null]
+    lockBoard = false
+}
+
+setUpGame()
+
+
+
+
+
+
